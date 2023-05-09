bd0d9fd15e574a76facf621a1c33b1a5c0ae281e
vanilanorca <vilopesalves@hotmail.com>
2023-05-09 17:44:49 -0300

    ajuste webkit

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
 var loader = document.getElementsByClassName('loading');
 var content = document.getElementsByClassName("page-content");
 
-loader[0].style.display = 'flex';
+loader[0].style.display = 'none';
+// MUDAR PARA FLEX
 
 var timer = setTimeout(function() {
   loader[0].style.display = 'none';
